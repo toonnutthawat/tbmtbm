29d93533892efe00cf0a639c5e258a96a396f910
Nutthawat Aonprasert <nutthawat.ao@ku.th>
2023-10-08 21:57:23 +0700

    first commit

---
+++ src/test/java/ku/cs/tbm/TbmApplicationTests.java
@@ -0,0 +1,13 @@
+package ku.cs.tbm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
